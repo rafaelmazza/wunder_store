d3caad4c0a63775d4feeaf6fa66e61307761cd5c
Rafael Mazza <rafael@cafeazul.com.br>
2012-05-26 11:51:26 -0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'mongoid', '~> 2.4'
+gem 'bson_ext', '~> 1.5'
+
+group :development do
+end
+
+group :test do
+	gem 'rspec-rails', '~> 2.9.0'
+	gem 'capybara'
+	gem 'database_cleaner', '~> 0.7.2'
+	gem 'factory_girl_rails', '~> 3.1.0'
+	gem 'guard-rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WunderStore::Application.config.secret_token = '20474d91318eb025f0b74c04b05b5db2a360b6690d98db252acd19ac2a12e4f07930186e1a8fd0aa8b4b5c9d60081362669653ec90ab83c0ebdfcf8e1bcedc75'
